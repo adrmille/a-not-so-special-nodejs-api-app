a8e09cc0eaa319c4ec09d57bd1f3c56418b895d0
Adrian Mille <adrmille@users.noreply.github.com>
2019-10-06 19:41:21 +0200

    #2 You can now retrieve a JWT token from the login endpoint and use it to call the user endpoint

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
+const config = require('../config/config.js');
 
 /**
  * @swagger
@@ -10,7 +12,7 @@ const router = express.Router();
  *     produces:
  *       - application/json
  *     parameters:
- *       - name: username
+ *       - name: userId
  *         description: Username to use for login.
  *         in: formData
  *         required: true
@@ -23,9 +25,23 @@ const router = express.Router();
  *     responses:
  *       200:
  *         description: Login
+ *       401:
+ *         description: No such user found
  */
-router.post('/login', function(req, res) {
-  res.send('respond with a resource');
+router.post('/login', (req, res) => {
+  if(req.body.userId && req.body.password){
+    var userId = req.body.userId;
+    // var password = req.body.password;
+  } else {
+    res.status(401).json({message:"no such user found"});
+  }
+  // TODO sanitize request inputs
+  // TODO replace by a database call, to fetch user credentials
+
+  const payload = {id: userId};
+  const token = jwt.sign(payload, global.gConfig.jwt_secret_key);
+  // TODO database call to store the token
+  res.json({message: "ok", token: token});
 });
 
 module.exports = router;
