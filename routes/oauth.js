75ab106c732fd80dfe86eaafc1ef3df2eac6d3ff
Adrian Mille <adrmille@users.noreply.github.com>
2019-10-06 13:38:13 +0200

    #1 Add swagger dependency, swagger api definition generation from code

---
+++ routes/oauth.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+// FIXME this is just an example
+/**
+ * @swagger
+ *
+ * /login:
+ *   post:
+ *     description: Login to the application
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: username
+ *         description: Username to use for login.
+ *         in: formData
+ *         required: true
+ *         type: string
+ *       - name: password
+ *         description: User's password.
+ *         in: formData
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: login
+ */
+router.get('/token', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+module.exports = router;
