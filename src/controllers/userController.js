df387333e567654ff391f7dd4af7ad478542880d
Adrian Mille <adrmille@users.noreply.github.com>
2019-10-09 16:01:27 +0200

    #14 Add endpoint entry to 'Get user data filtered by user name'

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -13,3 +13,17 @@ exports.findUserById = async (req, res) => {
     res.status(500).send(error)
   }
 };
+
+exports.findUserByName = async (req, res) => {
+  try {
+    const user = await User.findOne({ name: req.params.name });
+    if (user) {
+      res.send(user);
+    } else {
+      res.status(401).send({error: 'user not found'})
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error)
+  }
+};
--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -32,4 +32,30 @@ router.get('/:userId', passport.authenticate('jwt', {session: false}),
     authorize.roles(Role.ADMIN, Role.USER),
     userController.findUserById);
 
+/**
+ * @swagger
+ *
+ * /users/name/{name}:
+ *   get:
+ *     security:
+ *       - bearerAuth: []
+ *     description: User data filtered by user id
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: name
+ *         description: The name of the user.
+ *         in: path
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: User data as json
+ *       401:
+ *         description: Unauthorized
+ */
+router.get('/name/:name', passport.authenticate('jwt', {session: false}),
+    authorize.roles(Role.ADMIN, Role.USER),
+    userController.findUserByName);
+
 module.exports = router;
